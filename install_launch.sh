ac082c8a07146ca25d03476ba62ebc5252aa1334
Sean Ng <sng3051@gmail.com>
2025-05-17 00:22:23 +0800

    install script update with dependencies

--- install_launch.sh
+++ install_launch.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -ex
 sudo yum install -y python3 stress git pip
 sudo amazon-linux-extras install epel -y
-amazon-linux-extras install epel
 pip3 install -r requirements.txt
-yum -y install stress
 
 export PHOTOS_BUCKET=g15-assignment-2
 export AWS_DEFAULT_REGION=us-east-1
 export DYNAMO_MODE=1
 export DATABASE_DB_NAME=Employees-2
 
-FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=5000
+FLASK_APP=application.py flask app run --host=0.0.0.0 --port=5000
 
-
-
-cat database_create_tables.sql | \
-mysql -h $$DATABASE_HOST -u $$DATABASE_USER -p$$DATABASE_PASSWORD
